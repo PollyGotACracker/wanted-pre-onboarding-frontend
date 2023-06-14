3c7b4e8c32819276b18c054e49ad5c1cacda65c4
PollyGotACracker <bitterns96@gmail.com>
2023-06-14 23:48:46 +0900

    fix: Todo useCallback 추가 및 delete 연속 클릭 방지

--- src/pages/User/SignInPage.jsx
+++ src/pages/User/SignInPage.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import Main from "../../components/atoms/Main";
+import { useCallback, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import Main from "../../components/atoms/Main";
 import UserForm from "../../components/templates/UserForm";
 import { ERROR_AUTH } from "../../constants/message";
 import { useAuthContext } from "../../contexts/authContext";
@@ -12,18 +12,21 @@ const SignInPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const onClickSubmit = async ({ email, password }) => {
-    setIsLoading(true);
-    const token = await signIn({ email, password });
-    if (token) {
-      setToken({ token, email });
-      userSignIn(email);
-      navigate("/todo", { replace: true });
-    } else {
-      setIsLoading(false);
-      return ERROR_AUTH.signIn;
-    }
-  };
+  const onClickSubmit = useCallback(
+    async ({ email, password }) => {
+      setIsLoading(true);
+      const token = await signIn({ email, password });
+      if (token) {
+        setToken({ token, email });
+        userSignIn(email);
+        navigate("/todo", { replace: true });
+      } else {
+        setIsLoading(false);
+        return ERROR_AUTH.signIn;
+      }
+    },
+    [setToken, userSignIn, navigate]
+  );
 
   return (
     <Main className={"sign-in"} color={"point"}>

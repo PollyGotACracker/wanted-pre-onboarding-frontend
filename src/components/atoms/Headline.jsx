5725a84b26826fcda67050cae232e7d0bce4611d
PollyGotACracker <bitterns96@gmail.com>
2023-08-20 23:20:26 +0900

    Updates

--- src/components/atoms/Headline.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-import "./Headline.css";
-
-const Headline = memo(({ text }) => {
-  return <h1 className="headline">{text}</h1>;
-});
-
-export default Headline;
--- src/components/molecules/WrapInput.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-import "./WrapInput.css";
-
-const WrapInput = memo(({ children }) => {
-  return <div className="wrap-input">{children}</div>;
-});
-
-export default WrapInput;
